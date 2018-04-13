f25e59b392d4b86334abaf299422b9e8e0160b35
Andreas Mattis Gullberg Hansen <goldenproduktionsdk@gmail.com>
2018-04-13 11:20:32 +0200

    Hash object class

---
+++ webForm/Hash.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace webForm
+{
+    public class Hash
+    {
+        public Hash()
+        {
+        }
+
+        public string HashMethode(string inputPassword)
+        {
+            string passwordSource = inputPassword;
+            
+                string hash = GetMd5Hash(inputPassword);
+                return hash;
+        }
+
+        public string GetMd5Hash(string input)
+        {
+            byte[] data = MD5.Create().ComputeHash(Encoding.UTF8.GetBytes(input));
+            StringBuilder sBuilder = new StringBuilder();
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+            return sBuilder.ToString();
+        }
+    }
+}
