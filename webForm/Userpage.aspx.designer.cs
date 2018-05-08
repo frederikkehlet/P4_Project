99d79960f758c353a149945df6204ce1ae524bba
Andreas Mattis Gullberg Hansen <goldenproduktionsdk@gmail.com>
2018-05-08 14:28:03 +0200

    prøv nu

--- webForm/Userpage.aspx.designer.cs
+++ webForm/Userpage.aspx.designer.cs
@@ -155,5 +155,14 @@ namespace webForm {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.DataList DataList1;
+        
+        /// <summary>
+        /// Literal1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal Literal1;
     }
 }
